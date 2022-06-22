271b48e9b38ba1a7b116c703786a0c1253b65f68
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-06-22 09:44:30 +0530

    n=int(input()) for i in range (n):     a=int(input())     d=0     b=1     while a:         r=a%10         a//=10         d+=r*b         b*=8     x=(bin(d)[2:])     print(x)

---
+++ Octal_to_Binary.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range (n):
+    a=int(input())
+    d=0
+    b=1
+    while a:
+        r=a%10
+        a//=10
+        d+=r*b
+        b*=8
+    x=(bin(d)[2:])
+    print(x)
