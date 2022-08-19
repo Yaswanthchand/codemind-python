245ba9b614525d1cf6aa5688e154b5b6849dfd09
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-08-19 23:33:44 +0530

    a=input() b=input() a1="" b1="" for i in a:     if i.isupper():         a1+=i.lower()     else:         a1+=i for i in b:     if i.isupper():         b1+=i.lower()     else:         b1+=i arr=list(a1.split()) brr=list(b1.split()) res=[] for i in brr:     if i in arr:         if i not in res:             res.append(i) if len(res)==0:     print(0) else:     for i in res:         print(i,end=" ")

--- common_words.py
+++ common_words.py
@@ -1,12 +1,26 @@
-s1=input()
-s2=input()
-m=s1.lower()
-n=s2.lower()
-m1=m.split()
-n1=n.split()
-y=[]
-for i in n1:
-    if i in m1:
-        y.append(i)
-print(*y)
-
+a=input()
+b=input()
+a1=""
+b1=""
+for i in a:
+    if i.isupper():
+        a1+=i.lower()
+    else:
+        a1+=i
+for i in b:
+    if i.isupper():
+        b1+=i.lower()
+    else:
+        b1+=i
+arr=list(a1.split())
+brr=list(b1.split())
+res=[]
+for i in brr:
+    if i in arr:
+        if i not in res:
+            res.append(i)
+if len(res)==0:
+    print(0)
+else:
+    for i in res:
+        print(i,end=" ")
