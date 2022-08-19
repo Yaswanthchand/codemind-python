283066fa3bf4328983d636ec61fa714cd6eb536f
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-08-19 21:53:31 +0530

    import math def prime(a):     c=0     for i in range(2,round(math.sqrt(a))+1):         if a%i==0:             return 0     return 1 a=int(input()) b=int(input()) c1=0 for i in range(a,b+1):     if prime(i)==1 and i!=1:         c1+=1 print(c1)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,13 +1,14 @@
 import math
 def prime(a):
-    for i in range(2,int(math.sqrt(a)+1)):
+    c=0
+    for i in range(2,round(math.sqrt(a))+1):
         if a%i==0:
             return 0
     return 1
 a=int(input())
 b=int(input())
-c=0
+c1=0
 for i in range(a,b+1):
     if prime(i)==1 and i!=1:
-        c+=1
-print(c)
+        c1+=1
+print(c1)
