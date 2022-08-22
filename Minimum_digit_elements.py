a243d8736be538fbd90f0f2d2f2ab9ac72083f52
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-08-22 22:38:44 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     c=0     if a==0:         b.append(i)         continue     while(i):         c+=1         i//=10     b.append(c) k=min(b) print(b.count(k))

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,14 +1,14 @@
-def fun(n):
-    c=0
-    while(n):
-        r=n%10
-        c+=1
-        n//=10
-    return c
-x=int(input())
+n=int(input())
 a=list(map(int,input().split()))
 b=[]
 for i in a:
-    b.append(fun(i))
-m=min(b)
-print(b.count(m))
+    c=0
+    if a==0:
+        b.append(i)
+        continue
+    while(i):
+        c+=1
+        i//=10
+    b.append(c)
+k=min(b)
+print(b.count(k))
