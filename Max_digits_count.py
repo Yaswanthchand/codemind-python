d3faac9f018cbcda3852400af3d80f8f3beca263
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-08-22 22:43:31 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     c=0     if a==0:         b.append(1)         continue     while(i):         c+=1         i//=10     b.append(c) k=max(b) print(b.count(k))

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,14 +1,14 @@
-def fun(n):
-    c=0
-    while(n):
-        r=n%10
-        c+=1
-        n//=10
-    return c
-x=int(input())
+n=int(input())
 a=list(map(int,input().split()))
 b=[]
 for i in a:
-    b.append(fun(i))
-m=max(b)
-print(b.count(m))
+    c=0
+    if a==0:
+        b.append(1)
+        continue
+    while(i):
+        c+=1
+        i//=10
+    b.append(c)
+k=max(b)
+print(b.count(k))
