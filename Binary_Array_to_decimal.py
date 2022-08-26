575d3e97786545a966a10e722a35250781cc33db
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-08-26 22:45:26 +0530

    n=int(input()) a=list(map(int,input().split())) a=a[::-1] s=0 k=0 for i in a:     s=s+(i*(2**k))     k+=1 print(s)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,14 +1,9 @@
-t=int(input())
+n=int(input())
 a=list(map(int,input().split()))
-s=[str(i) for i in a]
-x= "".join(s)
-r=int(x)
-d=0
-b=1
-while(r):
-    rem=r%10
-    d=d+rem*b
-    b*=2
-    r//=10
-print(d)
-    
+a=a[::-1]
+s=0
+k=0
+for i in a:
+    s=s+(i*(2**k))
+    k+=1
+print(s)
