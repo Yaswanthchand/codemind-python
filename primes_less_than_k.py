663aaf38a113a76c0d5a057e7dcfe7dd05b34d62
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-10-10 22:42:35 +0530

    def isprime(n):     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) x=int(input()) sum=c=0 for i in a:     if i==1:         continue     if(isprime(i)):         if(i<=x):             c+=1 print(c)

--- primes_less_than_k.py
+++ primes_less_than_k.py
@@ -1,16 +1,17 @@
-def prime(n):
+def isprime(n):
     for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+        if(n%i==0):
             return 0
     else:
         return 1
 n=int(input())
 a=list(map(int,input().split()))
-s=int(input())
-c=0
-for i in range(n):
-    if a[i]==1:
+x=int(input())
+sum=c=0
+for i in a:
+    if i==1:
         continue
-    if(prime(a[i])) and a[i]<=s:
-        c+=1
-print(c)
+    if(isprime(i)):
+        if(i<=x):
+            c+=1
+print(c)    
