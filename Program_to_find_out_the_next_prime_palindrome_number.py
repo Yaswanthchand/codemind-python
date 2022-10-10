b544cf40b1c206ee5c7e879d7a8e980433824821
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-10-10 20:30:01 +0530

    def prime(n):     for i in range(2,int((n**0.5)+1)):         if(n%i==0):             return 0     else:         return 1 def palindrome(n):     temp=n     s=0     while(n>0):         l=n%10         s=s*10+l         n=n//10     if(s==temp):         return 1     else:         return 0 n=int(input()) for i in range(n+1,100000):     if(prime(i) and palindrome(i)):         print(i)         break

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,26 +1,22 @@
 def prime(n):
-    for i in range(2,n):
-        if n%i==0:
+    for i in range(2,int((n**0.5)+1)):
+        if(n%i==0):
             return 0
     else:
         return 1
-def palindrome(k):
-    t=k
-    r=0
-    while(t):
-        d=t%10
-        r=r*10+d
-        t=t//10
-    if r==k:
+def palindrome(n):
+    temp=n
+    s=0
+    while(n>0):
+        l=n%10
+        s=s*10+l
+        n=n//10
+    if(s==temp):
         return 1
     else:
         return 0
 n=int(input())
-for i in range(n+1,10000000):
-    if prime(i):
-        if palindrome(i):
-            print(i)
-            break
-        
-        
-        
+for i in range(n+1,100000):
+    if(prime(i) and palindrome(i)):
+        print(i)
+        break
