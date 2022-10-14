84dadd01e4088435ed28adfa54e83dc8f1f4d78a
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-10-15 00:20:39 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     if(i<0):         i=i*(-1)     if(i==0):         i=1     c=0     while(i):         x=i%10         i=i//10         c+=1     b.append(c) print(*b)

--- count_digits.py
+++ count_digits.py
@@ -1,14 +1,15 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range(n):
+b=[]
+for i in a:
+    if(i<0):
+        i=i*(-1)
+    if(i==0):
+        i=1
     c=0
-    if a[i]==0:
-        c=1
-    if a[i]<0:
-        a[i]=abs(a[i])
-    while a[i]>0:
-        r=a[i]%10
+    while(i):
+        x=i%10
+        i=i//10
         c+=1
-        a[i]//=10
-    print(c,end=' ')
-        
+    b.append(c)
+print(*b)
