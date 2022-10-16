fc7070f79386ec1f9a533236c4a1015aa39e3a5f
Yaswanth Chand <98638890+Yaswanthchand@users.noreply.github.com>
2022-10-16 15:05:56 +0530

    n=int(input()) a=list(map(int,input().split())) c=d=0 for i in range(n):     if(i%2==1):         if(a[i]%2==1):             c+=1 for i in a:     if(i%2==1):         d+=1 if(c==d):     print(True) else:     print(False)

--- Strictly_ODD.py
+++ Strictly_ODD.py
@@ -1,14 +1,15 @@
 n=int(input())
 a=list(map(int,input().split()))
-y=[]
-for i in range(len(a)):
-    if a[i]%2==1:
-        y.append(i)
-c=0
-for j in y:
-    if j%2==1:
-        c+=1
-if(c==len(y)):
+c=d=0
+for i in range(n):
+    if(i%2==1):
+        if(a[i]%2==1):
+            c+=1
+for i in a:
+    if(i%2==1):
+        d+=1
+if(c==d):
     print(True)
 else:
     print(False)
+    
